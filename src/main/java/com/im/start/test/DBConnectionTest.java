d27207a638be2ca9fea7aa636b42a5939446712c
rootgit23 <rmsgh1009@naver.com>
2022-08-03 11:09:27 +0900

    TEST

--- src/main/java/com/im/start/test/DBConnectionTest.java
+++ src/main/java/com/im/start/test/DBConnectionTest.java
@@ -1,9 +1,19 @@
 package com.im.start.test;
 
+import java.sql.Connection;
+
+import com.im.start.util.DBConnector;
+
 public class DBConnectionTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			Connection con = DBConnector.getConnection();
+			System.out.println(con != null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
