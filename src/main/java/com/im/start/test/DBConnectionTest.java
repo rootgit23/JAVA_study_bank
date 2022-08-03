1e26ad1353a992c564ea1d5b567e93e31e71e7f2
rootgit23 <rmsgh1009@naver.com>
2022-08-03 10:40:27 +0900

    init

---
+++ src/main/java/com/im/start/test/DBConnectionTest.java
@@ -0,0 +1,10 @@
+package com.im.start.test;
+
+public class DBConnectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/im/start/test/TestMain.java
@@ -0,0 +1,10 @@
+package com.im.start.test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
